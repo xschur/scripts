5a80cb6200554445d920f817a2ea2e6f5311db5a
schur <ring0sec@gmail.com>
2020-01-30 18:35:38 +0800

    update

--- get_title.py
+++ get_title.py
@@ -1,20 +1,20 @@
 import asyncio
 import re
-
+import sys
 import aiohttp
 
 PATTERN = re.compile(r'\<title\>(?P<title>.*)\<\/title\>')
 
 
 async def fetch_page(session, url):
-    async with session.get(url, ssl=False) as resp:
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.13; rv:67.0) Gecko/20100101 Firefox/67.0'}
+    async with session.get(url, headers=headers,ssl=False) as resp:
         return await resp.text()
-
 
 async def show_title(url):
     async with aiohttp.ClientSession() as session:
         html = await fetch_page(session, url)
-        print(PATTERN.search(html).group('title')+'----'+url)
+        print(PATTERN.search(html).group('title')+' ---- '+url)
 
 
 def main():
@@ -29,6 +29,7 @@ def main():
     tasks = [show_title(url) for url in url_s]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
+    sys.exit()
 
 if __name__ == '__main__':
     main()
